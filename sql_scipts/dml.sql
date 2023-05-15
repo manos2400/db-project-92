61bee6535359bc538b60eda17b64b4e9bbe9197b
Manos Kolydakis <manos1939@gmail.com>
2023-05-15 10:26:37 +0300

    revert

--- sql_scipts/dml.sql
+++ sql_scipts/dml.sql
@@ -7,89 +7,52 @@ VALUES
 ("admin1", "password4", "Sarah Lee", "1987-11-05", "admin1@school.edu", "111 Elm St", "555-1111", "admin", NULL),
 ("admin2", "password5", "David Chen", "1975-02-28", "admin2@school.edu", "222 Pine St", "555-2222", "admin", NULL);
 
--- Insert students
 INSERT INTO users (username, password, real_name, date_of_birth, email, address, phone_number, type, total_books)
 VALUES
-(tmolohan0,E2jUyN,Molohan,Tilda,03/08/1994,tmolohan0@ibm.com,5894 Pennsylvania Alley,682-497-2433,teacher,5)
-(mlasham1,U1JWk3mfS,Lasham,Mata,22/03/1983,mlasham1@washingtonpost.com,5496 Claremont Pass,658-265-0874,teacher,4)
-(jcowin2,AVMhUcuE5UIM,Cowin,Jeniffer,16/09/1989,jcowin2@ycombinator.com,62 Charing Cross Park,194-915-9766,teacher,15)
-(bdomesday3,MDzh2ajKL8R5,Domesday,Berny,18/02/1984,bdomesday3@cmu.edu,864 Westport Place,782-895-8539,teacher,32)
-(fyendall4,ohNZYr,Yendall,Floyd,06/11/1989,fyendall4@symantec.com,64 Express Street,443-714-6123,teacher,15)
-(bbremeyer5,6NWGQVvkUFJ,Bremeyer,Brittney,21/07/1979,bbremeyer5@pinterest.com,02 Gerald Park,661-723-5273,teacher,13)
-(sstarmore6,8AlDfI5J5Y,Starmore,Sybila,27/11/1996,sstarmore6@time.com,35 Pond Avenue,763-724-0973,teacher,11)
-(ggiacomi7,B5HPFF,Giacomi,Grove,08/04/1982,ggiacomi7@samsung.com,6762 Independence Road,811-746-8457,teacher,5)
-(tpalfrey8,8b1SMGgCBvyr,Palfrey,Townsend,10/12/1984,tpalfrey8@ocn.ne.jp,17 Brickson Park Hill,285-536-4648,teacher,7)
-(ldewitt9,SY8f3SjORCcO,Dewitt,Luca,21/07/1986,ldewitt9@ucoz.ru,07637 Anderson Avenue,858-187-6643,teacher,8)
-(awellanda,v4pUAkFbrim4,Welland,Alberik,03/04/1983,awellanda@bloglovin.com,9461 Maywood Junction,448-551-6099,teacher,8)
-(ajupeb,eO4Z3r8,Jupe,Aline,05/10/1996,ajupeb@whitehouse.gov,00403 Myrtle Place,843-179-5456,teacher,7)
-(slatusc,Et2OnA7,Latus,Shari,28/12/1979,slatusc@jimdo.com,2149 Maryland Road,165-687-9178,teacher,6)
-(fclissd,o0SUi0,Cliss,Florenza,25/05/1988,fclissd@oracle.com,7 West Trail,415-816-5254,teacher,5)
-(jtumilitye,Fa7q6dhO1ac,Tumility,Jayme,10/04/1974,jtumilitye@networksolutions.com,26 Grover Crossing,939-258-1076,teacher,3)
-(hwintringhamf,fftcxoJO1S,Wintringham,Homere,18/11/1972,hwintringhamf@mapquest.com,9 Portage Junction,241-425-3374,teacher,23)
-(talenichicovg,aslpcmkC30,Alenichicov,Tanney,22/09/1976,talenichicovg@cloudflare.com,094 8th Plaza,294-385-8749,teacher,20)
-(mpollakh,iffOcbR8E,Pollak,Marlie,04/11/1978,mpollakh@163.com,789 Lerdahl Pass,644-794-6814,teacher,10)
-(adensoni,oyqOogY8ri,Denson,Angy,21/07/1994,adensoni@deliciousdays.com,04 Huxley Alley,243-428-8448,teacher,9)
-(dgilloranj,AsE7bRQ3,Gilloran,Dallas,16/01/1983,dgilloranj@cnbc.com,642 Fieldstone Parkway,896-620-8995,teacher,8)
-(aoloinnk,aw4xjxfP7ykz,Loinn,Amby,21/07/1987,aoloinnk@example.com,189 Prairieview Road,789-117-4076,teacher,41)
-(hmalickil,C1MtjykC,Malicki,Hermy,26/08/1973,hmalickil@tinyurl.com,53439 Marquette Way,169-262-7967,teacher,5)
-(swenderothm,mw6q7HlpQIO0,Wenderoth,Sapphire,25/07/1986,swenderothm@artisteer.com,06836 La Follette Alley,999-675-3146,teacher,11)
-(cmyfordn,6tCMmHYzRLJ,Myford,Catlee,17/12/1988,cmyfordn@behance.net,3288 Northfield Park,424-327-9439,teacher,13)
-(llaimableo,PUuhOt0tK,Laimable,Lesly,09/11/1993,llaimableo@bloglines.com,76 Bartelt Alley,479-304-6263,teacher,25)
-(emushartp,s3QUiKdL,Mushart,Eben,03/05/1975,emushartp@devhub.com,82 Cherokee Trail,258-348-7719,teacher,35)
-(dbraznellq,mhZBODTby,Braznell,Dulci,19/06/1983,dbraznellq@ucoz.com,2 Sunfield Lane,531-288-0500,teacher,15)
-(ifinkler,twWTsfiZL,Finkle,Irwinn,15/11/1996,ifinkler@japanpost.jp,3506 Heath Plaza,948-382-6566,teacher,12)
-(ngyrgorcewicxs,GzjHhTN1V,Gyrgorcewicx,Nichole,05/02/2000,ngyrgorcewicxs@tripod.com,519 Meadow Ridge Alley,223-855-8526,teacher,65)
-(nrattenberiet,C5xJac4NE,Rattenberie,Niki,28/04/1984,nrattenberiet@yahoo.co.jp,4 Spaight Trail,748-756-0711,teacher,32)
-(ufortesquieu0,1AlEt0,Fortesquieu,Urbanus,08/09/2006,ufortesquieu0@mit.edu,1 4th Park,755-961-3045,student,3)
-(cminer1,Zms1RO,Miner,Cornela,24/09/2020,cminer1@cornell.edu,787 Erie Point,666-343-5957,student,17)
-(sstorrier2,4YLixGd,Storrier,Stormie,07/09/2021,sstorrier2@tumblr.com,00 Gina Pass,728-938-4538,student,8)
-(zcavanaugh3,RYDTOBpL2,Cavanaugh,Zitella,29/01/2008,zcavanaugh3@last.fm,918 Vahlen Plaza,691-844-3443,student,17)
-(hlaweles4,S871Vg1v5a4,Laweles,Huey,25/09/2006,hlaweles4@umich.edu,095 Delladonna Pass,369-330-2170,student,14)
-(dlethebridge5,waWF87,Lethebridge,Debby,06/11/2020,dlethebridge5@va.gov,98125 Sunbrook Hill,143-141-2562,student,6)
-(clamba6,pFqBlWv,Lamba,Coreen,27/10/2018,clamba6@nasa.gov,49 Miller Center,614-213-9441,student,7)
-(sdanford7,iuaCsWCSu,Danford,Selestina,20/08/2010,sdanford7@wikispaces.com,892 Arapahoe Point,562-704-0809,student,18)
-(aspringett8,hGrrrXbmjN4K,Springett,Arabele,15/10/2016,aspringett8@hc360.com,8393 Vernon Avenue,481-140-2446,student,6)
-(ibrecknell9,XJJ14IfH,Brecknell,Isacco,03/05/2019,ibrecknell9@patch.com,39 Dovetail Drive,882-369-6362,student,11)
-(bdemeada,R7PyndAQjch,Demead,Blaine,10/11/2012,bdemeada@mapquest.com,69521 Daystar Crossing,563-162-1924,student,2)
-(lkennedyb,NIomYrkWbqI,Kennedy,Libby,29/11/2012,lkennedyb@nature.com,50 Pine View Pass,628-392-7728,student,23)
-(jdunniomc,lx2M4q3vzFrX,Dunniom,Joscelin,29/01/2007,jdunniomc@economist.com,09 Sachs Center,183-391-2570,student,2)
-(vhedgemand,gsiYGoT5TZCY,Hedgeman,Vail,13/10/2022,vhedgemand@csmonitor.com,9 Briar Crest Road,341-182-7190,student,12)
-(aadelmane,FaVShQ4LkWI,Adelman,Addia,01/12/2019,aadelmane@squarespace.com,5231 Luster Trail,215-248-1326,student,16)
-(vlampelf,cf7Q5f6N,Lampel,Vite,03/07/2004,vlampelf@arizona.edu,58 Kensington Way,610-804-5286,student,21)
-(ggeatorg,1No4FXV3nLr,Geator,Godfrey,26/07/2013,ggeatorg@unesco.org,669 Dottie Drive,424-402-9042,student,17)
-(bwicklingh,O0RmWAxs5,Wickling,Berti,06/10/2015,bwicklingh@cbslocal.com,0202 Erie Road,241-788-9774,student,14)
-(rgillyatti,EmOOJff2,Gillyatt,Ruprecht,20/06/2022,rgillyatti@twitpic.com,5481 Little Fleur Lane,557-152-5660,student,21)
-(sbilyardj,yymqTB,Bilyard,Shermie,09/01/2019,sbilyardj@buzzfeed.com,25735 Dryden Crossing,955-529-4222,student,14)
-(heldridk,0y9mnBC4Xh,Eldrid,Hillard,27/12/2021,heldridk@smugmug.com,09 Milwaukee Avenue,717-388-3350,student,30)
-(mhurranl,MBAlN3OWm,Hurran,Magdalene,20/11/2018,mhurranl@homestead.com,86 Steensland Street,576-336-2534,student,9)
-(gmcorkillm,vbuzrY,McOrkill,Garnet,09/10/2022,gmcorkillm@archive.org,07698 Linden Circle,709-321-5627,student,2)
-(maylorn,H4JQ02hvdrcB,Maylor,Leonid,26/11/2005,lmaylorn@sina.com.cn,09 Grim Crossing,208-308-8728,student,23)
-(hvanhaleno,FpU6uVViP0,Van Halen,Hal,31/10/2007,hvanhaleno@tamu.edu,31 David Parkway,708-727-6914,student,20)
-(dlawliep,mvS1QjhSc9d,Lawlie,Danella,19/12/2016,dlawliep@census.gov,9 Gateway Way,126-406-3348,student,7)
-(akasterq,hmQ0lS21E9w,Akaster,Constantina,10/03/2013,cakasterq@mapquest.com,47301 Fairview Avenue,188-920-8744,student,10)
-(dstqueninr,6uhlnQW,St Quenin,Delmar,07/12/2012,dstqueninr@upenn.edu,54728 Ilene Alley,550-229-5280,student,9)
-(perkinsons,h0GitP4qh,Perkinson,Hewe,22/02/2014,hperkinsons@gmpg.org,102 Golf Course Pass,879-501-2263,student,20)
-(kmarrablet,bRBvzH56,Marrable,Katinka,23/06/2015,kmarrablet@google.com,28 Westerfield Center,905-200-5515,student,27)
-(hdrippu,knvKLudjgbW5,Dripp,Henrietta,26/10/2009,hdrippu@squidoo.com,04 Cascade Hill,933-779-9607,student,2)
-(cgatfieldv,l01hio,Gatfield,Christel,21/12/2016,cgatfieldv@go.com,9 John Wall Plaza,231-665-3105,student,14)
-(oturrillw,COwcAVeQJQG,Turrill,Ole,05/06/2011,oturrillw@fastcompany.com,3 Emmet Drive,679-452-4871,student,11)
-(bcoskerryx,s4WRQj440l,Coskerry,Bail,09/11/2015,bcoskerryx@artisteer.com,0020 Lunder Parkway,868-968-5262,student,2)
-(hambroixy,rKWkitbpAq,Ambroix,Harley,29/05/2019,hambroixy@google.ru,73 Little Fleur Park,856-729-1952,student,1)
-(dlindroosz,x3mvTpbzCO,Lindroos,Diahann,14/02/2007,dlindroosz@google.es,84345 Rigney Trail,840-249-2405,student,7)
-(hdingsdale10,YDht42wM,Dingsdale,Hasty,13/04/2012,hdingsdale10@nyu.edu,02 Johnson Road,470-108-9419,student,25)
-(opail11,ILrBTDEW2,Pail,Ozzy,29/07/2020,opail11@cam.ac.uk,7832 Garrison Hill,249-352-1410,student,27)
-(jbreffitt12,T9yVQfOBc,Breffitt,Jilli,06/05/2015,jbreffitt12@hibu.com,1 Morning Circle,373-722-5013,student,21)
-(klilburn13,UMSp3bhnlJ,Lilburn,Keelia,12/08/2010,klilburn13@flickr.com,736 Summerview Alley,719-871-2448,student,19)
-(abanaszkiewicz14,JpfxmPLmb,Banaszkiewicz,Aldo,20/12/2003,abanaszkiewicz14@tripod.com,48 Bluestem Road,292-654-3924,student,15)
-(bkeen15,pq5caf,Keen,Bat,26/02/2019,bkeen15@opensource.org,86 Ramsey Lane,190-768-3748,student,12)
-(edenkin16,yKCdcWT,Denkin,Emlynne,30/12/2003,edenkin16@domainmarket.com,233 Summit Pass,857-606-7422,student,11)
-(jniblock17,53a4Xg2mQux,Niblock,Jena,24/02/2020,jniblock17@oakley.com,06001 Ridge Oak Trail,623-587-8164,student,14)
-(mkerwick18,eC1FQmOXF,Kerwick,Milli,02/08/2021,mkerwick18@twitpic.com,457 Armistice Point,333-121-5463,student,16)
-(dwilloughey19,7Ur0OuiK,Willoughey,Dale,06/01/2015,dwilloughey19@senate.gov,68527 Fremont Circle,585-464-4866,student,30)
-(ivinnicombe1a,Da4rMVgZGPRe,Vinnicombe,Ibby,07/11/2004,ivinnicombe1a@imgur.com,9125 Fremont Place,574-729-2512,student,29)
-(tklossek1b,AjLoDYv5ymu,Klossek,Temple,27/12/2012,tklossek1b@constantcontact.com,0811 Schlimgen Avenue,513-939-7727,student,19)
-(ihawse1c,XqTcUnD,Hawse,Ilario,03/07/2010,ihawse1c@newyorker.com,880 Marquette Way,452-376-2375,student,18)
-(dswanger1d,7jahUL,Swanger,Dyane,10/11/2012,dswanger1d@ucoz.ru,275 Bluejay Circle,716-316-8433,student,11);
+("student1", "password6", "Emily Davis", "2007-09-01", "student1@school.edu", "123 Main St", "555-1234", "student", 3),
+("student2", "password7", "Jacob Rodriguez", "2006-10-12", "student2@school.edu", "456 Oak St", "555-5678", "student", 0),
+("student3", "password8", "Madison Martinez", "2008-03-21", "student3@school.edu", "789 Maple Ave", "555-9012", "student", 2),
+("student4", "password9", "Ethan Johnson", "2006-05-30", "student4@school.edu", "111 Elm St", "555-1111", "student", 1),
+("student5", "password10", "Isabella Martinez", "2007-01-02", "student5@school.edu", "222 Pine St", "555-2222", "student", 4),
+("student6", "password11", "Michael Lee", "2006-08-14", "student6@school.edu", "333 Maple St", "555-3333", "student", 2),
+("student7", "password12", "Sophia Chen", "2007-03-27", "student7@school.edu", "444 Cedar St", "555-4444", "student", 0),
+("student8", "password13", "Daniel Brown", "2008-04-15", "student8@school.edu", "555 Oak St", "555-5555", "student", 3),
+("student9", "password14", "Mia Taylor", "2007-06-08", "student9@school.edu", "666 Maple St", "555-6666", "student", 1),
+("student10", "password15", "William Rodriguez", "2006-02-09", "student10@school.edu", "777 Elm St", "555-7777", "student", 0),
+("student11", "password16", "Chloe Garcia", "2007-09-17", "student11@school.edu", "888 Pine St", "555-8888", "student", 2),
+("student12", "password17", "Andrew Perez", "2006-11-20", "student12@school.edu", "999 Maple St", "555-9999", "student", 4),
+("student13", "password18", "Abigail Hernandez", "2008-01-01", "student13@school.edu", "1010 Cedar St", "555-1010", "student", 0),
+("student14", "password19", "Joshua Smith", "2006-07-07", "student14@school.edu", "1111 Oak St", "555-1112", "student", 1),
+("student15", "password20", "Emma Nguyen", "2007-02-14", "student15@school.edu", "1212 Maple St", "555-1212", "student", 3),
+("student16", "password21", "Christopher Wilson", "2006-09-23", "student16@school.edu", "1313 Elm St", "555-1313", "student", 0),
+("student17", "password22", "Olivia Davis", "2007-05-06", "student17@school.edu", "1414 Pine St", "555-1414", "student", 2),
+("student18", "password23", "David Rodriguez", "2006-12-15", "student18@school.edu", "1515 Maple St", "555-1515", "student", 1),
+("student19", "password24", "Ava Martinez", "2008-02-22", "student19@school.edu", "1616 Cedar St", "555-1616", "student", 2),
+("student20", "password25", "Elijah Thompson", "2007-10-03", "student20@school.edu", "1717 Oak St", "555-1717", "student", 0),
+("student21", "password26", "Emily Perez", "2006-04-07", "student21@school.edu", "1818 Maple St", "555-1818", "student", 1),
+("student22", "password27", "Aiden Garcia", "2007-11-11", "student22@school.edu", "1919 Elm St", "555-1919", "student", 3),
+("student23", "password28", "Madison Hernandez", "2006-09-14", "student23@school.edu", "2020 Pine St", "555-2020", "student", 2),
+("student24", "password29", "Jackson Davis", "2007-06-17", "student24@school.edu", "2121 Maple St", "555-2121", "student", 0),
+("student25", "password30", "Avery Wilson", "2006-02-20", "student25@school.edu", "2222 Cedar St", "555-2222", "student", 1),
+("student26", "password31", "Caleb Brown", "2008-03-23", "student26@school.edu", "2323 Oak St", "555-2323", "student", 4),
+("student27", "password32", "Ella Lee", "2007-01-26", "student27@school.edu", "2424 Maple St", "555-2424", "student", 1),
+("student28", "password33", "Lucas Chen", "2006-08-29", "student28@school.edu", "2525 Elm St", "555-2525", "student", 0),
+("student29", "password34", "Natalie Taylor", "2007-04-02", "student29@school.edu", "2626 Pine St", "555-2626", "student", 2),
+("student30", "password35", "Mason Martinez", "2006-12-05", "student30@school.edu", "2727 Maple St", "555-2727", "student", 3);
+
+-- Insert Teachers
+INSERT INTO users (username, password, real_name, date_of_birth, email, address, phone_number, type, total_books)
+VALUES
+("teacher1", "password36", "Maria Garcia", "1985-09-03", "teacher1@school.edu", "1010 Oak St", "555-1010", "teacher", 0),
+("teacher2", "password37", "David Brown", "1983-05-07", "teacher2@school.edu", "1111 Maple St", "555-1111", "teacher", 2),
+("teacher3", "password38", "Angela Rodriguez", "1984-11-11", "teacher3@school.edu", "1212 Elm St", "555-1212", "teacher", 1),
+("teacher4", "password39", "James Wilson", "1982-09-14", "teacher4@school.edu", "1313 Pine St", "555-1313", "teacher", 0),
+("teacher5", "password40", "Sarah Smith", "1985-06-17", "teacher5@school.edu", "1414 Maple St", "555-1414", "teacher", 3),
+("teacher6", "password41", "Juan Hernandez", "1983-02-20", "teacher6@school.edu", "1515 Cedar St", "555-1515", "teacher", 0),
+("teacher7", "password42", "Emily Davis", "1986-03-23", "teacher7@school.edu", "1616 Oak St", "555-1616", "teacher", 1),
+("teacher8", "password43", "Anthony Lee", "1984-01-26", "teacher8@school.edu", "1717 Maple St", "555-1717", "teacher", 2),
+("teacher9", "password44", "Jessica Chen", "1983-08-29", "teacher9@school.edu", "1818 Elm St", "555-1818", "teacher", 1),
+("teacher10", "password45", "Ryan Taylor", "1985-04-02", "teacher10@school.edu", "1919 Pine St", "555-1919", "teacher", 3);
 
 -- Insert schools
 INSERT INTO schools (name, address, city, phone_number, email, principal_name, library_manager_id)
