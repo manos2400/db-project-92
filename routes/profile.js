481a0bc948c4cba427b37a4a699a0a5ee595d456
Manos Kolydakis <manos1939@gmail.com>
2023-05-21 12:01:00 +0300

    refactor

--- routes/profile.js
+++ routes/profile.js
@@ -3,37 +3,33 @@ const pool = require('../database.js');
 const router = express.Router()
 
 router.get("/", async (req, res) => {
-    if (!req.session.loggedIn) { return res.redirect('/'); }
-    // Retrieve session information
-    const connection = await pool.getConnection();
-    let school, user;
     try {
-        const rows = await connection.query(`SELECT * FROM schools WHERE id = ?;`, [req.session.school.id]);
-        if (rows.length !== 0) {
-            school = rows[0];
-        } else {
+        if (!req.session.loggedIn) {
+            return res.redirect('/');
+        }
+
+        const connection = await pool.getConnection();
+        const [[school]] = await connection.query(`SELECT * FROM schools WHERE id = ?;`, [req.session.school.id]);
+        if (!school) {
             throw new Error('School not found');
         }
-        const users = await connection.query(`SELECT * FROM users WHERE id = ?;`, [req.session.user.id]);
-        if (rows.length !== 0) {
-            user = users[0];
-        } else {
+
+        const [[user]] = await connection.query(`SELECT * FROM users WHERE id = ?;`, [req.session.user.id]);
+        if (!user) {
             throw new Error('User not found');
         }
 
+        await connection.release();
+
+        return res.render('profile', {
+            session: req.session,
+            user,
+            school
+        });
     } catch (error) {
         console.error(error);
         return res.status(503).send('Database is currently unavailable.');
-    } finally {
-        await connection.release();
     }
-
-    // Render the profile view and pass session information as locals
-    return res.render('profile', {
-        session: req.session,
-        user,
-        school
-    });
-})
+});
 
 module.exports = router;
