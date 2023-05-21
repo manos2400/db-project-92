5f8a05440c7e63879b417248d3870a0f13ff1157
Manos Kolydakis <manos1939@gmail.com>
2023-05-21 12:02:21 +0300

    dev

--- routes/books.js
+++ routes/books.js
@@ -4,40 +4,28 @@ const pool = require('../database.js');
 const router = express.Router()
 
 router.get("/", async (req, res) => {
-    if (req.session.loggedIn) {
-        try {
-            // Retrieve school_id from session information
-            const schoolID = req.session.school_id;
-
-            // Fetch books from school_books table based on school_id
-
-            const connection = await pool.getConnection();
-
-            try {
-                // Check credentials
-                const rows = await connection.query(`SELECT * FROM school_books WHERE school_id = ?`, [schoolID]);
-                req.session.books = []
-                for (var i = 0; i < rows.length; i++) {
-                    const books = await connection.query(`SELECT * FROM books WHERE id = ?`, [rows[i].book_id]);
-                    req.session.books.push(books[0]);
-                }
-            } catch (error) {
-                console.error(error);
-                return res.status(500).send('Internal Server Error');
-            } finally {
-                await connection.release();
-            }
-
-            // Render the books view and pass the fetched books as locals
-            return res.render('books', { books: req.session.books });
-        } catch (error) {
-            console.error(error);
-            // Render an error view or send an error response
-            return res.status(500).send('Internal Server Error');
-        }
-    } else {
-        return res.redirect('/');
-    }
+    return res.status(501).send('Not implemented');
 })
+router.get("/:id", async (req, res) => {
+    try {
+        if (!req.session.loggedIn) {
+            return res.redirect('/');
+        }
 
+        const connection = await pool.getConnection();
+        const book = await connection.query(`SELECT * FROM books WHERE id = ?;`, [req.params.id]);
+        if (!book) {
+            return res.status(404).send('Book not found');
+        }
+        await connection.release();
+
+        return res.render('profile', {
+            session: req.session,
+            book: book[0]
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(503).send('Database is currently unavailable.');
+    }
+});
 module.exports = router;
