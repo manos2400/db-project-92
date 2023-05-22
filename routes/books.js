65268a53a02a873915100787fd6d917e3f47e396
Manos Kolydakis <manos1939@gmail.com>
2023-05-22 20:40:08 +0300

    Added books page

--- routes/books.js
+++ routes/books.js
@@ -4,25 +4,39 @@ const pool = require('../database.js');
 const router = express.Router()
 
 router.get("/", async (req, res) => {
-    return res.status(501).send('Not implemented');
+    try {
+        if (!req.session.loggedIn) {
+            return res.redirect('/');
+        }
+        const connection = await pool.getConnection();
+        const books = await connection.query(`SELECT id, picture FROM school_books_view WHERE school_id = ?;`, [req.session.school.id]);
+        if (!books) {
+            throw new Error('No books found');
+        }
+        await connection.release();
+
+        return res.render('books', {
+            session: req.session,
+            books
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(503).send('Database is currently unavailable.');
+    }
 })
 router.get("/:id", async (req, res) => {
     try {
         if (!req.session.loggedIn) {
             return res.redirect('/');
         }
-
         const connection = await pool.getConnection();
-        const book = await connection.query(`SELECT * FROM books WHERE id = ?;`, [req.params.id]);
+        const book = await connection.query(`SELECT * FROM school_books_view WHERE id = ? AND school_id = ?;`, [req.params.id, req.session.school.id]);
         if (!book) {
             return res.status(404).send('Book not found');
         }
         await connection.release();
 
-        return res.render('profile', {
-            session: req.session,
-            book: book[0]
-        });
+        return res.send(book[0]);
     } catch (error) {
         console.error(error);
         return res.status(503).send('Database is currently unavailable.');
