0bb5bd6eb787261b62bb3cbac26a3fbbaccde0c1
Manos Kolydakis <manos1939@gmail.com>
2023-06-03 10:40:09 +0300

    Added all functionalities in schools page

--- routes/admin/schools.js
+++ routes/admin/schools.js
@@ -24,14 +24,52 @@ router.get("/:id", async (req, res) => {
 
   const connection = await pool.getConnection();
   const schools = await connection.query(`
-  SELECT schools.*, users.real_name AS manager_name
-  FROM schools 
-  INNER JOIN users ON schools.library_manager_id = users.id 
-  WHERE schools.id = ?;
+    SELECT s.*, u.real_name AS manager_name
+    FROM schools s
+    JOIN school_users su ON s.id = su.school_id
+    JOIN users u ON u.id = su.user_id
+    WHERE u.type = 'manager'; 
   `, [req.params.id]);   
   await connection.release();
 
-  return res.render("admin/schools", { session: req.session, schools });
+  return res.send(schools[0]);
+});
+router.post("/edit/:id", async (req, res) => {
+    if (!req.session.loggedIn || req.session.user.type !== "admin") { return res.redirect('/'); }
+    try {
+        const connection = await pool.getConnection();
+        const { name, address, city, phone_number, email, principal_name } = req.body;
+
+        await connection.query(`
+            UPDATE schools 
+            SET name = ?, address = ?, city = ?, phone_number = ?, email = ?, principal_name = ?
+            WHERE id = ?;
+        `,[name, address, city, phone_number, email, principal_name, req.params.id]);
+        await connection.release();
+
+        return res.status(200).send("School updated successfully.");
+    } catch (error) {
+        console.error(error);
+        return res.status(503).send("Database is currently unavailable.");
+    }
+});
+router.post("/create/", async (req, res) => {
+    if (!req.session.loggedIn || req.session.user.type !== "admin") { return res.redirect('/'); }
+    try {
+        const connection = await pool.getConnection();
+        const { name, address, city, phone_number, email, principal_name } = req.body;
+
+        await connection.query(`
+            INSERT INTO schools (name, address, city, phone_number, email, principal_name) 
+            VALUES (?, ?, ?, ?, ?, ?);
+            `,[name, address, city, phone_number, email, principal_name]);
+        await connection.release();
+
+        return res.status(200).send("School created successfully.");
+    } catch (error) {
+        console.error(error);
+        return res.status(503).send("Database is currently unavailable.");
+    }
 });
 
 module.exports = router;
