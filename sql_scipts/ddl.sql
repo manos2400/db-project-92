1798a9e9710ee4f705b237c6e444e7f07d9be684
Manos Kolydakis <manos1939@gmail.com>
2023-06-04 10:17:57 +0300

    fixes

--- sql_scipts/ddl.sql
+++ sql_scipts/ddl.sql
@@ -186,3 +186,75 @@ BEGIN
     WHERE user_id = userId;
 END //
 DELIMITER ;
+
+-- Triggers
+DELIMITER //
+CREATE TRIGGER `new_loan` AFTER INSERT ON `loans` 
+FOR EACH ROW 
+BEGIN 
+IF NEW.date_in IS NULL THEN
+UPDATE school_books 
+SET available = available - 1 
+WHERE book_id = NEW.book_id AND school_id = NEW.school_id;
+END IF;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER `wait_reservations` AFTER UPDATE ON `school_books` 
+FOR EACH ROW 
+BEGIN 
+IF NEW.available = 0 THEN UPDATE reservations 
+SET waited = TRUE
+WHERE book_id = NEW.book_id AND school_id = NEW.school_id; END IF; 
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER `unwait_reservations` AFTER UPDATE ON `school_books` 
+FOR EACH ROW 
+BEGIN 
+IF NEW.available > 0 THEN UPDATE reservations 
+SET waited = FALSE, date = CURRENT_DATE(), date_due = DATE_ADD(CURRENT_DATE(), INTERVAL 7 DAY) 
+WHERE book_id = NEW.book_id AND school_id = NEW.school_id; 
+END IF; 
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER `check_availability` BEFORE INSERT ON `reservations` 
+FOR EACH ROW 
+BEGIN 
+  DECLARE copies INT; 
+
+  SELECT available INTO copies FROM school_books 
+  WHERE book_id = NEW.book_id AND school_id = NEW.school_id; 
+
+  IF copies = 0 THEN 
+    SET NEW.waited = TRUE; 
+  END IF; 
+END //
+DELIMITER ; 
+
+DELIMITER //
+CREATE TRIGGER `safe_user_delete` BEFORE DELETE ON `users` 
+FOR EACH ROW 
+BEGIN 
+DELETE FROM reservations WHERE user_id = old.id; 
+DELETE FROM school_users WHERE user_id = old.id; 
+DELETE FROM loans WHERE user_id = old.id; 
+DELETE FROM reviews WHERE user_id = old.id; 
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER `returned_loan` AFTER UPDATE ON `loans` 
+FOR EACH ROW 
+BEGIN
+IF NEW.date_in IS NOT NULL THEN 
+UPDATE school_books 
+SET available = available + 1 
+WHERE book_id = NEW.book_id AND school_id = NEW.school_id;
+END IF;
+END //
+DELIMITER ;
