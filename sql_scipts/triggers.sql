dd0c975d4d4c2da5ac33a22333cf61d84aed63fd
Manos Kolydakis <manos1939@gmail.com>
2023-06-04 10:12:58 +0300

    fix

--- sql_scipts/triggers.sql
+++ sql_scipts/triggers.sql
@@ -1,4 +1,5 @@
 -- Triggers
+DELIMITER //
 CREATE TRIGGER `new_loan` AFTER INSERT ON `loans` 
 FOR EACH ROW 
 BEGIN 
@@ -7,16 +8,20 @@ UPDATE school_books
 SET available = available - 1 
 WHERE book_id = NEW.book_id AND school_id = NEW.school_id;
 END IF;
-END 
+END //
+DELIMITER ;
 
+DELIMITER //
 CREATE TRIGGER `wait_reservations` AFTER UPDATE ON `school_books` 
 FOR EACH ROW 
 BEGIN 
 IF NEW.available = 0 THEN UPDATE reservations 
 SET waited = TRUE
 WHERE book_id = NEW.book_id AND school_id = NEW.school_id; END IF; 
-END
+END //
+DELIMITER ;
 
+DELIMITER //
 CREATE TRIGGER `unwait_reservations` AFTER UPDATE ON `school_books` 
 FOR EACH ROW 
 BEGIN 
@@ -24,8 +29,10 @@ IF NEW.available > 0 THEN UPDATE reservations
 SET waited = FALSE, date = CURRENT_DATE(), date_due = DATE_ADD(CURRENT_DATE(), INTERVAL 7 DAY) 
 WHERE book_id = NEW.book_id AND school_id = NEW.school_id; 
 END IF; 
-END
+END //
+DELIMITER ;
 
+DELIMITER //
 CREATE TRIGGER `check_availability` BEFORE INSERT ON `reservations` 
 FOR EACH ROW 
 BEGIN 
@@ -37,8 +44,10 @@ BEGIN
   IF copies = 0 THEN 
     SET NEW.waited = TRUE; 
   END IF; 
-END; 
+END //
+DELIMITER ; 
 
+DELIMITER //
 CREATE TRIGGER `safe_user_delete` BEFORE DELETE ON `users` 
 FOR EACH ROW 
 BEGIN 
@@ -46,8 +55,10 @@ DELETE FROM reservations WHERE user_id = old.id;
 DELETE FROM school_users WHERE user_id = old.id; 
 DELETE FROM loans WHERE user_id = old.id; 
 DELETE FROM reviews WHERE user_id = old.id; 
-END
+END //
+DELIMITER ;
 
+DELIMITER //
 CREATE TRIGGER `returned_loan` AFTER UPDATE ON `loans` 
 FOR EACH ROW 
 BEGIN
@@ -56,4 +67,5 @@ UPDATE school_books
 SET available = available + 1 
 WHERE book_id = NEW.book_id AND school_id = NEW.school_id;
 END IF;
-END 
+END //
+DELIMITER ;
